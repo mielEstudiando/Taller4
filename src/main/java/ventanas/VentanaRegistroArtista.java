5da9c79fbca86182e12d709dc158f4ce7cb319d4
miel <m.ferrada09@ufromail.cl>
2023-12-07 16:38:28 -0300

    más interfaces gráficas horribles qUE NO QUIEREN QUEDARME SIMÉTRICAS

--- src/main/java/ventanas/VentanaRegistroArtista.java
+++ src/main/java/ventanas/VentanaRegistroArtista.java
@@ -1,5 +1,6 @@
 package ventanas;
 
+import model.Artista;
 import model.Genero;
 
 import javax.swing.*;
@@ -7,7 +8,7 @@ import java.awt.event.ActionEvent;
 
 public class VentanaRegistroArtista extends Ventana {
 
-    private JLabel textoMenu, textoNombre, textoGeneros;
+    private JLabel textoMenu, textoNombre;
     private JTextField campoNombre;
     private JComboBox listaGeneros;
     private JButton botonRegistro, botonAtras, botonSalir;
@@ -36,8 +37,6 @@ public class VentanaRegistroArtista extends Ventana {
     }
 
     private void generarListaGenero(){
-        String textoGenero = "Genero:";
-        super.generarJLabel(this.textoGeneros,textoGenero,20,100,150,20);
         this.listaGeneros = super.generarListaDesplegable(generos,20,100,150,20);
         this.add(this.listaGeneros);
     }
@@ -60,22 +59,29 @@ public class VentanaRegistroArtista extends Ventana {
         this.botonSalir.addActionListener(this);
     }
 
+    private Artista registrarCliente(Genero genero){
+        return new Artista(this.campoNombre.getText(), genero);
+    }
+
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == this.botonSalir) {
             //aquí se añadirá el sistema de guardado de datos cuando corresponda :)
             this.dispose();
             System.exit(0);
         }
-        if(e.getSource() == this.botonRegistrarArtista){
-            VentanaRegistroArtista ventanaRegistroArtista= new VentanaRegistroArtista();
-            this.dispose();
+        if(e.getSource() == this.botonRegistro){
+            if ((this.listaGeneros.getSelectedItem() != null)&&(!(this.campoNombre.getText().isEmpty()))) {
+                Genero seleccion = (Genero) listaGeneros.getSelectedItem();
+                registrarCliente(seleccion);
+                JOptionPane.showMessageDialog(this,"Artista registrado correctamente");
+                VentanaInicio ventanaInicio = new VentanaInicio();
+                this.dispose();
+            } else {
+                JOptionPane.showMessageDialog(this, "Error, ingrese correctamente los datos");
+            }
         }
-        if(e.getSource() == this.botonRegistrarEvento){
-            VentanaRegistroEvento ventanaRegistroEvento= new VentanaRegistroEvento();
-            this.dispose();
-        }
-        if(e.getSource() == this.botonRegistrarAsistente){
-            VentanaRegistroAsistente ventanaRegistroAsistente= new VentanaRegistroAsistente();
+        if(e.getSource() == this.botonAtras){
+            VentanaInicio ventanaInicio= new VentanaInicio();
             this.dispose();
         }
     }
